5000db1d98239a0661b5c7cb1619fd2f9f66c240
micha <micha@KOMPUTER>
2024-12-08 20:12:37 +0100

    Podstawowa konfiguracja głównej tabeli

--- Restauracja/Restauracja/Models/Database/RestaurantDbContext.cs
+++ Restauracja/Restauracja/Models/Database/RestaurantDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Restauracja.Models.Database.Entities;
 
 namespace Restauracja.Models.Database
 {
     public class RestaurantDbContext : DbContext
     {
+        public DbSet<DaneRestauracji> restauracjas {  get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             var daneDoLogowania = new ConfigurationBuilder()
@@ -20,7 +22,28 @@ namespace Restauracja.Models.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DaneRestauracji>(entity =>
+            {
+                entity.Property(x => x.Nazwa)
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                entity.Property(x => x.Opis)
+                        .HasMaxLength(350)
+                        .IsRequired();
+
+                entity.Property(x => x.GodzinaOtwarcia)
+                        .IsRequired();
+
+                entity.Property(x => x.GodzinaZamkniecia)
+                        .IsRequired();
+
+                entity.Property(x => x.CzasOtwarcia)
+                        .IsRequired();
+
+                entity.Property(x => x.CzyDostawa)
+                        .IsRequired();
+            });               
         }
     }
 }
