477f0ef3fdc45628da0ee05f809c22c006cfc9b0
micha <micha@KOMPUTER>
2024-12-08 19:51:06 +0100

    Dodanie EntityFramework Core (v. 9.0) do projektu

---
+++ Restauracja/Restauracja/Models/Database/RestaurantDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Restauracja.Models.Database
+{
+    public class RestaurantDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var daneDoLogowania = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = daneDoLogowania.Build();
+
+            string connection = configuration["ConnectionStrings:Default"] ?? string.Empty;
+
+            builder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
