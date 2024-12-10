42a5f0ceadfd39f47e348c2d53410be5d9d07dbf
micha <micha@KOMPUTER>
2024-12-10 22:14:47 +0100

    Podstawowa obsluga formularza

---
+++ Restauracja/Restauracja/Models/PrzyjmowaneDane/RestauracjaModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Restauracja.Models.PrzyjmowaneDane
+{
+    public class RestauracjaModel
+    {
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public string TypLokalu { get; set; }
+        public string TypRestauracji { get; set; }
+        public bool CzyDriveThru { get; set; }
+        public bool CzyDostawa { get; set; }
+        public bool CzyParkinPrzyRestauracji { get; set; } = false;
+        public bool CzyMozliwaRezerwacja { get; set; }
+        public bool CzyImprezyOkolicznosciowe { get; set; }
+        public bool CzySalaOkolicznosciowa { get; set; }
+        public DateTime GodzinaOtwarcia { get; set; }
+        public DateTime GodzinaZamkniecia { get; set; }
+        public TimeSpan CzasOtwarcia { get; set; }
+    }
+}
