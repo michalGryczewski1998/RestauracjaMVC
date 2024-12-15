03c22041ad056a287237594ffccdb1ab52f10e63
micha <micha@KOMPUTER>
2024-12-15 20:14:16 +0100

    Dodanie nowych encji

---
+++ Restauracja/Restauracja/Models/Database/Entities/AplikacjaInformacje.cs
@@ -0,0 +1,11 @@
+﻿namespace Restauracja.Models.Database.Entities
+{
+    public class AplikacjaInformacje
+    {
+        public Guid Id { get; set; }
+        public decimal WersjaAplikacji { get; set; }
+        public DateTime DataWersji { get; set; }
+        public string NazwaAplikacji { get; set; }
+        public string Opis { get; set; }
+    }
+}
